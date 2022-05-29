6c2c8692fc124b0f16accd4a8da99f43b879e475
Minh Hieu <minhhieuhuynh140@gmail.com>
2022-05-29 10:58:40 +0700

    config: add webpack to project

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  purge: [
+    "./build/*.html",
+    "./public/index.html",
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+  ],
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   important: true,
   theme: {
